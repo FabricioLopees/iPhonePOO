a455b359d8d5154ca24d18d5609e2a53485624ec
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-08-09 16:41:05 -0300

    feat: add app AparelhoTelefonico.java

---
+++ src/me/fabricio/iphone/poo/aplicativos/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package me.fabricio.iphone.poo.aplicativos;
+
+public interface AparelhoTelefonico{
+	abstract void ligar(String numero);
+	abstract void atender();
+	abstract void iniciarCorreioVoz();
+}
