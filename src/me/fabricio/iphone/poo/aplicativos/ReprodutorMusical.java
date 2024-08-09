da74b06150b84a78ce30c01505531491e40fcc21
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-08-09 16:40:16 -0300

    feat: add app ReprodutorMusical.java

---
+++ src/me/fabricio/iphone/poo/aplicativos/ReprodutorMusical.java
@@ -0,0 +1,7 @@
+package me.fabricio.iphone.poo.aplicativos;
+
+public interface ReprodutorMusical {
+	abstract void tocar();
+	abstract void pausar();
+	abstract void selecionarMusica(String musica);
+}
